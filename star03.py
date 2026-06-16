3ef5f550958aace42605294636734d5ec8159e73
vaishnavi-codes <vaishnavigupta88093@gmail.com>
2026-06-16 18:08:57 +0530

    Delete star03.py

--- star03.py
+++
@@ -1,9 +0,0 @@
-n=int(input("enter your number:"))
-for i in range(1,n+1):
-    if(i==1 or i==n):
-        print("*"*(n),ends="")
-    else:
-        print("*",end="")
-        print(" "*(n-2),end="")
-        print("*",end="")
-        print("")
