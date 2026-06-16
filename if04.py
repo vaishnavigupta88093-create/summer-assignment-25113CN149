47ec1cde8d0238ae00b544981e937f5321fb977d
vaishnavi-codes <vaishnavigupta88093@gmail.com>
2026-06-16 18:02:20 +0530

    Delete if04.py

--- if04.py
+++
@@ -1,6 +0,0 @@
-list=["vaishnavi","yashi","yatharth","anshuman","shreya","pavitra","atharva","preeti","pramod"]
-name=input("enter your name:")
-if name in list:
-    print("NAME FOUND")
-else:
-    print("NAME NOT FOUND")
