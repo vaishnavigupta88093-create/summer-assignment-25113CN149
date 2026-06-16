bb36cae1939dee3e67ddac69408616aa3e8acf5f
vaishnavi-codes <vaishnavigupta88093@gmail.com>
2026-06-16 17:59:37 +0530

    Delete file06.py

--- file06.py
+++
@@ -1,9 +0,0 @@
-with open("file06nnew.txt", "r",encoding="utf-8") as f:
-    data = f.read()
-
-data = data.replace("donkey", "#####")
-
-with open("file06nnew.txt", "w",encoding="utf-8") as f:
-    f.write(data)
-
-print("succesfully replaced")
