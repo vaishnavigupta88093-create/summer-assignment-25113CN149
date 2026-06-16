721c1bfda6d0ff16b2969ee24c8a60a9562eb09c
vaishnavi-codes <vaishnavigupta88093@gmail.com>
2026-06-16 18:07:44 +0530

    Delete spam.py

--- spam.py
+++
@@ -1,9 +0,0 @@
-comment=input("enter your comment:")
-comment=comment.lower()
-if( "make a lot of money" in comment or
-    "buy now" in comment or
-"click this " in comment or 
-"subscribe this " in comment):
-    print("SPAM")
-else:
-    print("SAFE")
