088f5e650ec4ad2b4bceaf245cc3e0f7e5c5dddd
vaishnavi-codes <vaishnavigupta88093@gmail.com>
2026-06-22 23:42:16 +0530

    Add files via upload

---
+++ day11/p01.py
@@ -0,0 +1,9 @@
+
+a=int(input("enter your number a:"))
+b=int(input("enter your number b:"))
+
+def sum(a,b):
+    sum=a+b
+    return sum
+print(sum(a,b))
+    
---
+++ day11/p04.py
@@ -0,0 +1,7 @@
+n=int(input("enter your number:"))
+
+def fact(n):
+    if(n==1 or n==0):
+        return 1
+    return n*fact(n-1)
+print(f"the factorial of A number is:{fact(n)}")
